27adab7d726e056e0c6bac9fc16c4863e6bcd2fc
Jonas <jonas.illipe@elev.ga.lba.se>
2021-02-23 14:26:42 +0100

    Patrol script enemy

---
+++ Game-Water/Assets/Scripts/Patrol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    public float rayLenght = 2f;
+
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+    
+    void Update()
+    {// fixa så att den kollar ned och höger
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, rayLenght);
+        if(groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0,-180,0);
+                movingRight = false;
+            } else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
+}

7a63e0317f67100964b0c74a867017f81f0c82e5
username-kevin <74253394+username-kevin@users.noreply.github.com>
2021-03-02 15:21:04 +0100

    Playerbullet
    yyes

---
+++ Game-Water/Assets/PlayerBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + 0.25f, transform.position.y, transform.position.z);
+    }
+}
---
+++ Game-Water/Assets/PlayerBulletShot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBulletShot : MonoBehaviour
+{
+    
+    Vector2 angle;
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+
+
+
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}

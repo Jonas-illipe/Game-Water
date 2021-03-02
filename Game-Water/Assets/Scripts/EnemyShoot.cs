b59dc05640163356309a27e33cf2c28196733305
Jonas <jonas.illipe@elev.ga.lba.se>
2021-03-02 14:39:01 +0100

    Enemy Bullets
    Yep bullet till enemys

---
+++ Game-Water/Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.WebCam;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public GameObject bullet;
+
+    float fireRate;
+    float nextFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {// sätter fire rate till 1 skott i sekunden
+        fireRate = 1f;
+        nextFire = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {//gör att CheckIfTimeToFire voiden startar varje frame precis som i update
+        CheckIfTimeToFire ();
+    }
+
+    void CheckIfTimeToFire()
+    { 
+        if (Time.time > nextFire)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}

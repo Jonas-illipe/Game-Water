7e50777a4a7d59055982994011715b24eb3f5d9f
Jonas <jonas.illipe@elev.ga.lba.se>
2021-03-09 15:56:31 +0100

    Bullet Koppling
    Han connectat bulleten till player. Behöver bara lägga till skadan på bulleten så är den färdig.

--- Game-Water/Assets/Scripts/EnemyBullet.cs
+++ Game-Water/Assets/Scripts/EnemyBullet.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
-{/*
-    GameObject Player = GameObject.FindGameObjectWithTag("Player");
-    
+{
+    PlayerMovement target;
+
     float moveSpeed = 7f;
 
     Rigidbody2D rb;
 
-    Player target;
     Vector2 moveDirection;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D> ();
-        target = GameObject.FindObjectOfType<Player>();
+        target = GameObject.FindObjectOfType<PlayerMovement>();
         moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
         rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
         Destroy (gameObject, 3f);
@@ -31,5 +30,5 @@ public class EnemyBullet : MonoBehaviour
             Debug.Log("Hit");
             Destroy (gameObject);
         }
-    } */
+    } 
 }

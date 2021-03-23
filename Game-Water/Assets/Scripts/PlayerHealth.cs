5cee5790a75af59e1c2b158df989b8fe951e2dae
username-kevin <74253394+username-kevin@users.noreply.github.com>
2021-03-23 14:42:47 +0100

    health and healthbar player
    ja

---
+++ Game-Water/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Slider healthslider;
+    // Start is called before the first frame update
+    public void SetMax(int health)
+    {
+        healthslider.maxValue = health;
+        healthslider.value = health;
+    }
+
+    // Update is called once per frame
+    public void SetHealth(int health)
+    {
+        healthslider.value = health;
+    }
+}
---
+++ Game-Water/Assets/Scripts/PlayerHealth2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth2 : MonoBehaviour
+{
+    public int Maxhealth = 200;
+    public int currenthealth;
+
+    public PlayerHealth healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currenthealth = Maxhealth;
+        healthBar.SetMax(Maxhealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+}

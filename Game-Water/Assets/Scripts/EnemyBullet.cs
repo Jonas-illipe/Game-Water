dc2ec6e8258cdda082dcdd5bde594a2b357138b5
Jonas <jonas.illipe@elev.ga.lba.se>
2021-03-23 15:56:58 +0100

    små fix

--- Game-Water/Assets/Scripts/EnemyBullet.cs
+++ Game-Water/Assets/Scripts/EnemyBullet.cs
@@ -25,7 +25,7 @@ public class EnemyBullet : MonoBehaviour
     // Update is called once per frame
     void OnTriggerEnter2D (Collider2D col)
     {
-        if (col.gameObject.name.Equals("Player"))
+        if (col.gameObject.name.Equals("player"))
         {
             Debug.Log("Hit");
             Destroy (gameObject);

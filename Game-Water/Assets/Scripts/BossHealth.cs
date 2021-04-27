343659ccba80307caae5e67a6fd2be20988f6027
username-kevin <74253394+username-kevin@users.noreply.github.com>
2021-04-27 14:00:48 +0200

    main manue
    sbnfiabehc

---
+++ Game-Water/Assets/Scripts/BossHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour
+{
+    public int bossMaxHealth = 400;
+    public int currentBossHealth;
+    public GameObject Boss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentBossHealth = bossMaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

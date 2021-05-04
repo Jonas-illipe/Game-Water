3eca29c63e414b46400cd6b165d26e6f98ff8adc
Jonas <jonas.illipe@elev.ga.lba.se>
2021-05-04 11:17:00 +0200

    Allt

---
+++ Game-Water/Assets/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new UnityEngine.Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
